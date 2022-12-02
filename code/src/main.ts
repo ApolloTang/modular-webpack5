80550e4427ac62d394f3bc83cbf479a343a310c4
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-12-02 15:07:08 -0500

    typo

--- code/src/main.ts
+++ code/src/main.ts
@@ -2,7 +2,7 @@ import '~/main.css'
 
 
 // [NOTE]
-//   To see fonts in action make sure you unzip monrope.zip in ./src/fonts/
+//   To see fonts in action make sure you unzip monrope.zip in src/fonts/
 import('~/fonts/').then(()=>{
   import('~/setup-check/setup-check--loading-fonts/')
 })
