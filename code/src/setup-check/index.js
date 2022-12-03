9c223e5b60f33390daeb9dcf86e9b630d0e77851
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-12-03 16:47:19 -0500

    refactor

--- code/src/setup-check/index.js
+++ code/src/setup-check/index.js
@@ -1,9 +1,14 @@
-import  './setup-check--purge-css/'
-import  './setup-check--loading-images/'
-import  './setup-check--js/'
-import  './setup-check--typescript/'
-import  './setup-check--hmr/'
-import  './setup-check--react/'
+import './main.css'
+import './setup-check--purge-css/'
+import './setup-check--loading-images/'
+import './setup-check--js/'
+import './setup-check--typescript/'
+import './setup-check--hmr/'
+import './setup-check--react/'
 
-
+// [NOTE]
+//   To see fonts in action make sure you unzip monrope.zip in src/fonts/
+import('~/fonts/').then(()=>{
+  import('~/setup-check/setup-check--loading-fonts/')
+})
 
